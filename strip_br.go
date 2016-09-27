830e2aa76af73d0c488e5bc44fed2f8da86467c0
Shlomi Noach <shlomi-noach@github.com>
2016-09-27 22:02:01 +0200

    more initial work

---
+++ strip_br.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	fileName := os.Args[1]
+	fmt.Println(fileName)
+	b, _ := ioutil.ReadFile(fileName)
+	re := regexp.MustCompile(`(?s)<blockquote>[\s]*<pre>(.*?)</pre>[\s]*</blockquote>`)
+	f := func(input []byte) []byte {
+		s := string(input)
+		s = strings.Replace(s, "<br />", "", -1)
+		s = strings.Replace(s, "<br/>", "", -1)
+		return []byte(s)
+	}
+	replaced := re.ReplaceAllFunc(b, f)
+	fmt.Println(string(replaced))
+}
