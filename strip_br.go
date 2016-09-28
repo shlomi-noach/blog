3fde0a90870d3f704b26cc4ca962ccba922c6ed4
Shlomi Noach <shlomi-noach@github.com>
2016-09-28 14:03:42 +0200

    More stripping

--- strip_br.go
+++ strip_br.go
@@ -10,15 +10,16 @@ import (
 
 func main() {
 	fileName := os.Args[1]
-	fmt.Println(fileName)
 	b, _ := ioutil.ReadFile(fileName)
 	re := regexp.MustCompile(`(?s)<blockquote>[\s]*<pre>(.*?)</pre>[\s]*</blockquote>`)
 	f := func(input []byte) []byte {
 		s := string(input)
 		s = strings.Replace(s, "<br />", "", -1)
 		s = strings.Replace(s, "<br/>", "", -1)
+		s = strings.Replace(s, "<p>", "", -1)
+		s = strings.Replace(s, "</p>", "", -1)
 		return []byte(s)
 	}
 	replaced := re.ReplaceAllFunc(b, f)
-	fmt.Println(string(replaced))
+	fmt.Print(strings.TrimSpace(string(replaced)))
 }
